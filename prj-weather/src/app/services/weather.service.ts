5cede1cc162ab42c6fc9099267e1830e39248bed
Ronaldo Junior <ronaldosjr2005@gmail.com>
2023-05-31 18:04:15 -0300

    T001: Criação do serviço que procura o clima das cidades por nome

--- prj-weather/src/app/services/weather.service.ts
+++ prj-weather/src/app/services/weather.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs';
+
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
 
+  private API_KEY = 'efcc78a8cc4316b2d8dee2a3f8d7aba6'
+  private API_URL = 'https://api.openweathermap.org/data/2.5/weather?q='
+
   constructor(private http: HttpClient) {  }
+
+    getWeatherByCityName(cityName: string): Observable<any>{
+      return this.http.get(`${this.API_URL}${cityName}&appid=${this.API_KEY}&lang=pt_br&units=metric`)
+    }
 }
