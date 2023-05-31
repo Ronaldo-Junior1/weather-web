61e0e5986daf8400f00b7fa207912d518e80ab99
Ronaldo Junior <ronaldosjr2005@gmail.com>
2023-05-31 18:07:37 -0300

    T001: Implementação do 'getgetWeatherByCityName' na 'home-page'

--- prj-weather/src/app/home-page/home-page.component.ts
+++ prj-weather/src/app/home-page/home-page.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
-
+import { WeatherService } from '../services/weather.service';
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent {
+  
+  constructor(private weatherService: WeatherService) {}
 
+  ngOnInit(){
+
+  }
+  cityName: string = "";
+
+  searchCity(){
+    if (this.cityName) {
+      this.weatherService.getWeatherByCityName(this.cityName).subscribe(
+        (data) => {
+          const cityName = data.name;
+          const temperature = data.main.temp;
+          
+          console.log("Cidade: " + cityName);
+          console.log("Temperatura: " + temperature);
+        },
+        (error) => {
+          console.log("Erro ao obter dados da API:", error);
+        }
+      );
+    } else {
+      console.log("Digite o nome da cidade antes de pesquisar.");
+    }
+  }
 }
