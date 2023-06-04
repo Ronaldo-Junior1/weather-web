eeb8cdb71c29d3023c064d127919bf84c87e44bf
Ronaldo Junior <ronaldosjr2005@gmail.com>
2023-06-04 18:23:01 +0000

    T001: Implementação das informações principais na tela e da função para arredondar a temperatura

--- prj-weather/src/app/home-page/home-page.component.ts
+++ prj-weather/src/app/home-page/home-page.component.ts
@@ -1,28 +1,22 @@
 import { Component } from '@angular/core';
 import { WeatherService } from '../services/weather.service';
+
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent {
-  
+  weatherData: any;
+  cityName: string = "";
   constructor(private weatherService: WeatherService) {}
 
-  ngOnInit(){
-
-  }
-  cityName: string = "";
-
-  searchCity(){
+  searchCity() {
     if (this.cityName) {
       this.weatherService.getWeatherByCityName(this.cityName).subscribe(
         (data) => {
-          const cityName = data.name;
-          const temperature = data.main.temp;
-          
-          console.log("Cidade: " + cityName);
-          console.log("Temperatura: " + temperature);
+          this.weatherData = data;
+          console.log(this.weatherData);
         },
         (error) => {
           console.log("Erro ao obter dados da API:", error);
@@ -31,5 +25,9 @@ export class HomePageComponent {
     } else {
       console.log("Digite o nome da cidade antes de pesquisar.");
     }
+  }
+
+  roundTemperature(temperature: number): number {
+    return Math.round(temperature);
   }
 }
