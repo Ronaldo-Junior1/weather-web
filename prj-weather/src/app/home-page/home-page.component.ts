14f2683bbd551dde3f98429f4a07cfc4954bfde4
Ronaldo Junior <ronaldosjr2005@gmail.com>
2023-06-05 21:49:21 +0000

    T002: Implementação das bandeiras dos paises das cidades pesquisadas

--- prj-weather/src/app/home-page/home-page.component.ts
+++ prj-weather/src/app/home-page/home-page.component.ts
@@ -11,6 +11,8 @@ export class HomePageComponent {
   cityName: string = "";
   errorMessage: string = "";
   weatherIconUrl: string = "";
+  countryFlagUrl : string = "";
+
   constructor(private weatherService: WeatherService) {}
 
   searchCity() {
@@ -22,18 +24,23 @@ export class HomePageComponent {
           this.errorMessage = "";
         
           const iconCode = this.weatherData.weather[0].icon;
+          const flagName = this.weatherData.sys.country;
+          
+          this.countryFlagUrl = `https://flagsapi.com/${flagName}/flat/64.png`;
           this.weatherIconUrl = `http://openweathermap.org/img/w/${iconCode}.png`;
         },
         (error) => {
           console.log("Nome da cidade invalido", error);
           this.errorMessage = "Nome da cidade invalido";
           this.weatherIconUrl="";
+          this.countryFlagUrl = "";
         }
       );
     } else {
       console.log("Digite o nome da cidade antes de pesquisar.");
       this.errorMessage = "Digite o nome da cidade antes de pesquisar";
       this.weatherIconUrl="";
+      this.countryFlagUrl= "";
     }
   }
 
